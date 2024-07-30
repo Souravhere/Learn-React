e1d2545bccd3e0b576a8bcde833e5b5566a2d8fa
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-07-30 19:08:00 +0530

    Theme switch few bugs solved

--- ThemeSwitch/src/App.jsx
+++ ThemeSwitch/src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useState } from "react"
+import { ThemeProvider } from "./context/Theme"
+import { useEffect } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+const [themeMode, setThemeMode] = useState("light")
+const lightTheme = ()=>{
+  setThemeMode("light")
+}
+const darkTheme = ()=>{
+  setThemeMode("dark")
+}
+// actual change in theme
+useEffect(()=>{
+  document.querySelector('html').classList.remove("light","dark")
+  document.querySelector('html').classList.add(themeMode)
+},[themeMode])
 
   return (
-    <>
-      <h1 className='text-2xl'>Hello</h1>
-    </>
+<ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+<div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+      <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+        {/* Theme Buttons */}
+      </div>
+      <div className="w-full max-w-sm mx-auto">
+        {/* Cards */}
+      </div>
+    </div>
+</div>
+</ThemeProvider>
   )
 }
 
