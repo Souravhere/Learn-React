8a1d26738a266aabe63c32d800e1d4b469f53c58
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-01 19:21:30 +0530

    Todo form added with the link with app.js

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Todoprovider } from './contexts';
+import TodoForm from './components/TodoForm';
 
 function App() {
     const [todos, setTodos] = useState([]);
@@ -49,6 +50,7 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */}
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/* Loop and Add TodoItem here */}
