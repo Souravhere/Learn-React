838586460a4eeb738ce99bc01326e599f9eeb56d
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-01 19:38:01 +0530

    ToDoApp completed

--- ToDoApp/src/components/TodoItem.jsx
+++ ToDoApp/src/components/TodoItem.jsx
@@ -5,7 +5,7 @@ function TodoItem({ todo }) {
     const [isTodoEditable, setIsTodoEditable] = useState(false)
     const [todoMsg, setTodoMsg] = useState(todo.todo)
 
-    const { deleteTodo, updatedTodo, toggleComplete} = useTodo
+    const { deleteTodo, updatedTodo, toggleComplete} = useTodo()
 
     const editTodo = () => {
         updatedTodo(todo.id, {...todo, todo: todoMsg})
